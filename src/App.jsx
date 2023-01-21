4cd1bf82f6edce1d9c240a626ca6c932cacab91e
JM Baldonado <john.baldonado@xurpas.com>
2023-01-21 21:08:47 +0800

    Add Logic for managing state of the cart and modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/layout/header/index.jsx";
 import Meals from "./components/meals/index.jsx";
 import Cart from "./components/cart/index.jsx";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/ui/modal/components/backdrop/index.jsx
+++ src/components/ui/modal/components/backdrop/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./styled.module.css";
-const Index = () => {
-  return <div className={classes.backdrop} />;
+const Index = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 export default Index;
